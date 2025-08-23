c7ee733ac8bd72696e7e4081dc3e4e73d9fa5dc4
Lens0021 / Leslie <lorentz0021@gmail.com>
2025-08-23 12:12:27 +0900

    Install all CIs (#4)
    * Add qqq.json

---
+++ .phan/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['directory_list'] = array_merge(
+	$cfg['directory_list'],
+	[]
+);
+
+$cfg['exclude_analysis_directory_list'] = array_merge(
+	$cfg['exclude_analysis_directory_list'],
+	[]
+);
+
+return $cfg;
